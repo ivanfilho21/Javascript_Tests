9d2c1b9686ab57c5121f9cda16585fb980b6ece7
Ivan Filho <ivanfilho21@gmail.com>
2020-08-19 20:37:55 -0300

    Add some concepts

--- projeto1/js/script.js
+++ projeto1/js/script.js
@@ -1,13 +1,24 @@
 var x = 10;
 var y = 15;
 var z = x + y;
-window.alert(z);
+// window.alert(z);
+
 
 // Encontra um elemento por Id e altera seu conteúdo interno
 document.getElementById('alterar_conteudo').innerHTML = 'Olá mundo';
 
+// Constante
+const grid_width = 120;
+
+// Hoisting: var fica acessível em toda a janela
+var age = 100
+
+// let fica disponível apenas no escopo atual (dentro do if, por exemplo)
+if (age <= 100) {
+    let name = 'Martin';
+}
 // Adiciona conteúdo no documento
-document.write('Texto qualquer');
+document.write('Idade: ' + window.age + ' anos');
 
 // Mostra mensagens no Console
 console.log('Mensagem comum exibida no Console.');
