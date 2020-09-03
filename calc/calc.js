23dc0374f2025e112310d39553d5be64b15bbaaa
Ivan Filho <ivanfilho21@gmail.com>
2020-09-03 09:46:00 -0300

    Refactor

--- calc/calc.js
+++ calc/calc.js
@@ -7,7 +7,7 @@ document.querySelector('.lp_container.b .result').innerHTML = lpB;
 window.onload = () => {
     let btnsA = document.querySelectorAll('.lp_container.a .btn');
     let btnsB = document.querySelectorAll('.lp_container.b .btn');
-    console.log(btnsA);
+    let values = document.querySelectorAll('input[name=value]');
 
     for (let btn of btnsA) {
         btn.onclick = () => updateLifePoints(btn, true);
@@ -16,13 +16,22 @@ window.onload = () => {
     for (let btn of btnsB) {
         btn.onclick = () => updateLifePoints(btn, false);
     }
+
+    for (let v of values) {
+        v.onfocus = () => selectAll(v);
+    }
+}
+
+function selectAll(input) {
+    input.selectionStart = 0;
+    input.selectionEnd = input.value.length;
 }
 
 function updateLifePoints(btn, firstPlayer = false) {
     let src = firstPlayer ? 'a' : 'b';
     let lp = firstPlayer ? lpA : lpB;
     let op = btn.dataset ? btn.dataset.operation : (btn.hasAttribute('data-operation') ? btn.getAttribute('data-operation') : null);
-    let value = document.querySelector(`input[name=value_${src}]`).value;
+    let value = document.querySelector(`.lp_container.${src} input[name=value]`).value;
     value = parseInt(value);
     let res = document.querySelector(`.lp_container.${src} .result`);
 
