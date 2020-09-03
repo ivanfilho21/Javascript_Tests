b812930068ec05f4e45ec829b452bd4584e09858
Ivan Filho <ivanfilho21@gmail.com>
2020-09-02 21:46:01 -0300

    Create functions to calc life points of player A

--- calc/calc.js
+++ calc/calc.js
@@ -1,3 +1,59 @@
+var lpA = 8000;
+var lpB = 8000;
+
+window.onload = () => {
+    let btnsA = document.querySelectorAll('.lp_container.a .btn');
+    let btnsB = document.querySelectorAll('.lp_container.b .btn');
+    console.log(btnsA);
+
+    for (let btn of btnsA) {
+        btn.onclick = () => updateLifePointsA(btn);
+    }
+
+    for (let btn of btnsB) {
+        // btn.onclick = () => lpB();
+    }
+}
+
+function updateLifePointsA(btn) {
+    let op = btn.dataset ? btn.dataset.operation : (btn.hasAttribute('data-operation') ? btn.getAttribute('data-operation') : null);
+    let value = document.querySelector('input[name=value_a]');
+    let res = document.querySelector('.lp_container.a .result');
+
+    if (! op) return;
+
+    switch (op) {
+        case 'sum':
+            res.innerHTML = lpA + parseInt(value.value);
+            break;
+        case 'subtract':
+            res.innerHTML = lpA - parseInt(value.value);
+            break;
+    }
+}
+/* 
+
+
+function test(parent) {
+    console.log(parent);
+    switch (parent) {
+        case 'lp_a':
+            console.log('A');
+            break;
+        case 'lp_b':
+            console.log('B');
+            break;
+    }
+    
+    let valueB = document.querySelector('input[name=value_b]').value;
+    let lpA = document.querySelector('input[name=lp_a]');
+    let lpB = document.querySelector('input[name=lp_b]');
+
+    alert('');
+    return false;
+}
+
+
 function calc() {
     let opA = document.querySelector('input[name=operator_a]').value;
     let opB = document.querySelector('input[name=operator_b]').value;
@@ -24,4 +80,4 @@ function calc() {
 
     console.log(res);
     document.querySelector('.result .value').innerHTML = res;
-}
+} */
