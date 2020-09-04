aa275c7d1641625362d2c6c8c790437a59cc7c6e
Ivan Filho <ivanfilho21@gmail.com>
2020-09-04 11:56:40 -0300

    Implement stack functions

--- data_structures/js/stack.js
+++ data_structures/js/stack.js
@@ -6,7 +6,29 @@ function push() {
     let value = stackData.value;
     if (! value) return;
 
+    // Adds 'value' to the 'last+1' index of array, deleting '0' elements
+    stack.splice(stack.length, 0, value);
     updateStack();
 }
 
-function updateStack() {}
+function pop() {
+    // Remove 1 element starting from the last position of array
+    stack.splice(stack.length -1, 1);
+    updateStack();
+}
+
+function updateStack() {
+    let stackDOM = document.querySelector('.stack');
+    let content = '';
+    let stackCopy = stack.slice().reverse();
+
+    for (let data of stackCopy) {
+        content += `
+            <div class="element">
+                <div class="data">${data}</div>
+            </div>
+        `;
+    }
+
+    stackDOM.innerHTML = content;
+}
